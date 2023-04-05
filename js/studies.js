2a1f8b4952db85b1d1f3ee738046d39df7c45d71
Vasyl <vasiliy.vs45@gmail.com>
2023-04-05 08:24:30 +0200

    task add

--- js/studies.js
+++ js/studies.js
@@ -57,43 +57,38 @@
 //     }
 // }
 // writeYourGeners();
-
 // function calculateVolumeAndArea(length) {
 //     if (typeof (length) !== 'number' || length < 0 || !Number.isInteger(length)) {
 //         return "При вычислении произошла ошибка";
 //     }
-
 //     let volume = 0,
 //         area = 0;
-
 //     volume = length * length * length;
 //     // length ** 3 - это тоже самое, что и выше или варианты через цикл.
 //     // ** - это оператор степени, напоминаю. Но онлайн редактор его не принимает =/
 //     area = 6 * (length * length);
-
 //     return `Объем куба: ${volume}, площадь всей поверхности: ${area}`;
 // }
 
 // calculateVolumeAndArea(5);
 
 
-// Место для второй задачи
+//  Создайте функцию, которая будет вычислять объем и площадь полной поверхности куба (тоже базовая математика, иногда используется в создании анимаций). Эта функция принимает в себя целое число со значением длины ребра куба. Ответ выведите в формате строки, который изображен в примерах.
+// Если в функцию попал неправильный аргумент или вычислить значения невозможно - вернуть строку 
 // function getCoupeNumber(numberCoope) {
 //     if ( typeof(numberCoope) !== 'number' || numberCoope == null || numberCoope <0 || !Number.isInteger(numberCoope) ){
 //         return "Ошибка. Проверьте правильность введенного номера места";
 //     }
-
 //     if (numberCoope === 0 || numberCoope > 36){
 //         return "Таких мест в вагоне не существует";
 //     }
-
 //     return Math.ceil(numberCoope / 4)
-
 // }
 
 // console.log(getCoupeNumber(5));
 
-// Место для первой задачи
+
+// Создайте функцию, которая принимает в себя целое число минут и возвращает время в нужном формате строки. (Смотри пример). Обратите внимание на окончание слова "час" - оно меняется в зависимости от цифры. Если вместо аргумента приходит не число, дробное или отрицательное число - функция возвращает строку "Ошибка, проверьте данные"
 function getTimeFromMinutes(timeNumber) {
 
     if (typeof(timeNumber) !== 'number' || timeNumber < 0 || !Number.isInteger(timeNumber)){
